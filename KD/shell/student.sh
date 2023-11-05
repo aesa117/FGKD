ed49cee5ac014b885da9b558f401a1ec21f7c4b1
somi <handong227@gmail.com>
2023-11-05 16:01:34 +0900

    modify: shell & file name

--- KD/shell/student.sh
+++ KD/shell/student.sh
@@ -7,3 +7,10 @@ do
 done
 
 # mustaD setting
+for data in cora citeseer pubmed
+do
+    for model in GCNII GAT GraphSAGE
+    do
+        python3 -u student_baseline.py --dataset ${data} --student ${model} --mustad true
+    done
+done
